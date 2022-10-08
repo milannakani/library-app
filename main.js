5444ec6895cb2ed517e2abbbba06ad22c39f2664
Milan Nakani <milannakani@gmail.com>
2022-10-08 17:45:49 +0530

    working on delete button

--- main.js
+++ main.js
@@ -4,55 +4,99 @@ function openForm() { //function to open the form
   let myLibrary = [];
 
 const defaultData = [
-    {bookname: "three mistakes of my life", author: "chetan bhagat", pages: "300", readButton: "true"}
-]
-const $bookName = document.querySelector("#bookname");
-const $author = document.querySelector("#author"); 
-const $pages  = document.querySelector("#pages");
-const $readButton = document.querySelector("#readButton");
-const $form = document.querySelector("form").addEventListener("submit", (e) => {
+    {bookname: "three", author: "chetan bhagat", pages: "300", status: "true"}
+];
+const bookName = document.querySelector("#bookname");
+const author = document.querySelector("#author"); 
+const pages  = document.querySelector("#pages");
+const status = document.querySelector("#status");
+const tableBody = document.querySelector("#tableBody");
+const form = document.querySelector("form");
+    form.addEventListener("submit", (e) => {
     e.preventDefault();
     addBookToLibrary();
     displayBooks();
     clearForm()
 });
 
-class libraryBooks {
-    constructor(bookname, author, pages, readButton) {
+function LibraryBooks (bookname, author, pages, status) {
         this.bookname = bookname;
         this.author = author;
         this.pages = pages;
-        this.readButton = readButton;
+        this.status = status;
+    };
+
+    const table = document.querySelector("table")
+                          .addEventListener("click", (e) => {
+    const currentTarget = e.target.parentNode.parentNode.childNodes[1];
+    if (e.target.innerHTML == "delete") {
+      if (confirm(`are you sure you want to delete ${currentTarget.innerText}`))
+        deleteBook(findBook(myLibrary, currentTarget.innerText));
     }
-}
+    if (e.target.classList.contains("status-button")) {
+      changeStatus(findBook(myLibrary, currentTarget.innerText));
+    }
+    updateLocalStorage();
+    displayBooks();
+  });  
+
+
 function addBookToLibrary () {
-    const newBook = new libraryBooks($bookName.value, $author.value, $pages.value, $readButton.value );
+    let newBook = new LibraryBooks(bookName.value, author.value, pages.value, status.value);
     myLibrary.push(newBook);
     updateLocalStorage();
 }
 function updateLocalStorage() {
     localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-    //library = JSON.parse(localStorage.getItem("library"));
 }
 function checkLocalStorage() {
     if (localStorage.getItem("myLibrary")) {
       myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
     } else {
-      myLibrary = DEFAULT_DATA;
+      myLibrary = defaultData;
     }
   }
+
+  function changeStatus(book) {
+    if (myLibrary[book].status === "read") {
+      myLibrary[book].status = "not read";
+    } else myLibrary[book].status = "read";
+  }
+
+  function deleteBook(currentBook) {
+    myLibrary.splice(currentBook, currentBook + 1);
+  }
+
+  function findBook(libraryArray, name) {
+    if (libraryArray.length === 0 || libraryArray === null) {
+      return;
+    }
+    for (book of libraryArray)
+      if (book.name === name) {
+        return libraryArray.indexOf(book);
+      }
+    }
+
   function displayBooks () {
     checkLocalStorage ();
-    $tableBody.innerHTML = "";
-    myLibrary.forEach((libraryBooks) => {
-        const htmlBook = 
+    tableBody.innerHTML = "";
+    myLibrary.forEach((app) => {
+        const htmlBook = `
         <tr>
-            <td>${libraryBooks.bookname}</td>
-            <td>${libraryBooks.author}</td>
-            <td>${libraryBooks.pages}</td>
-            <td>${libraryBooks.readButton}</td>
-        </tr>;
-        $tableBody.insertAdjacentHTML("afterbegin", htmlBook);
+            <td>${app.bookname}</td>
+            <td>${app.author}</td>
+            <td>${app.pages}</td>
+            <td><button class="status-button">${app.status}</button></td>
+            <td><button class="delete">delete</button></td>
+         </tr>`;
+        tableBody.insertAdjacentHTML("afterbegin", htmlBook);
     });
   }
   displayBooks();
+
+  function clearForm () {
+    bookname = "";
+    author = "";
+    pages = "";
+    readButton = "";
+  }
